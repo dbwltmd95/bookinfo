5b20bd6ad934f4bc393456f600fa01d529389cd6
Administrator <admin@example.com>
2023-01-02 16:17:56 +0900

    Update LibertyRestEndpoint.java

--- reviews/reviews-application/src/main/java/application/rest/LibertyRestEndpoint.java
+++ reviews/reviews-application/src/main/java/application/rest/LibertyRestEndpoint.java
@@ -114,7 +114,7 @@ public class LibertyRestEndpoint extends Application {
       if (ratings_enabled) {
         if (starsReviewer1 != -1) {
           //result += ", \"rating\": {\"stars\": " + starsReviewer1 + ", \"color\": \"" + star_color + "\"}";
-          result += ", \"rating\": {\"stars\": " + starsReviewer1 + ", \"color\": \"" + "black" + "\"}";
+          result += ", \"rating\": {\"stars\": " + starsReviewer1 + ", \"color\": \"" + "rad" + "\"}";
         }
         else {
           result += ", \"rating\": {\"error\": \"Ratings service is currently unavailable\"}";
@@ -129,7 +129,7 @@ public class LibertyRestEndpoint extends Application {
       if (ratings_enabled) {
         if (starsReviewer2 != -1) {
           //result += ", \"rating\": {\"stars\": " + starsReviewer2 + ", \"color\": \"" + star_color + "\"}";
-          result += ", \"rating\": {\"stars\": " + starsReviewer2 + ", \"color\": \"" + "black" + "\"}";
+          result += ", \"rating\": {\"stars\": " + starsReviewer2 + ", \"color\": \"" + "rad" + "\"}";
         }
         else {
           result += ", \"rating\": {\"error\": \"Ratings service is currently unavailable\"}";
